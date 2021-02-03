3a233f9418afea6c645738931d9f94c5a5d13d45
christoph groß <christoph.gross@uni-tuebingen.de>
2021-02-03 11:28:30 +0100

    Use Error of simulator to create output

--- examples/marketplace_sim.rs
+++ examples/marketplace_sim.rs
@@ -11,7 +11,9 @@ pub fn main() /*-> Result<()>*/
     //The local addres mosaik connects to.
     let addr = "127.0.0.1:3456"; //wenn wir uns eigenständig verbinden wollen -> addr als option. accept_loop angepasst werden!!!
     let simulator = MarketplaceSim::init_sim();
-    run_simulation(addr, simulator);
+    if let Err(e) = run_simulation(addr, simulator) {
+        error!("{:?}", e);
+    }
 }
 
 impl MosaikAPI for MarketplaceSim {
