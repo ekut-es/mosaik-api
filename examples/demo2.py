fb420f4c7d424ff94aea4e013dc98f2ff6cde94a
Lukas Seehuber <lukas.seehuber@student.uni-tuebingen.de>
2024-08-15 10:18:11 +0200

    copy up-to-date python code from [official tutorial](https://mosaik.readthedocs.io/en/3.3.3/tutorials/demo2.html)

--- examples/demo2.py
+++ examples/demo2.py
@@ -2,38 +2,45 @@
 import mosaik
 import mosaik.util
 
-# Sim config. and other parameters
+
+# Sim config
 SIM_CONFIG = {
     "ExampleSim": {
-        "cmd": "cargo run --example example_sim -r -q -- -a=%(addr)s",
+        "python": "simulator_mosaik:ExampleSim",
     },
     "ExampleCtrl": {
-        "cmd": "cargo run --example controller -r -q -- -a=%(addr)s",
+        "python": "controller:Controller",
     },
     "Collector": {
-        "cmd": "%(python)s examples/collector.py %(addr)s",
+        "cmd": "%(python)s collector.py %(addr)s",
     },
 }
 END = 10  # 10 seconds
 
 # Create World
-world = mosaik.World(SIM_CONFIG, max_loop_iterations=3000)
+world = mosaik.World(SIM_CONFIG)
+# End: Create World
 
 # Start simulators
-examplesim = world.start("ExampleSim", eid_prefix="RModel_")
-examplectrl = world.start("ExampleCtrl")
+with world.group():
+    examplesim = world.start("ExampleSim", eid_prefix="Model_")
+    examplectrl = world.start("ExampleCtrl")
 collector = world.start("Collector")
+# End: Start simulators
 
 # Instantiate models
-models = [examplesim.RModel(init_val=i) for i in range(-2, 3, 2)]
+models = [examplesim.ExampleModel(init_val=i) for i in range(-2, 3, 2)]
 agents = examplectrl.Agent.create(len(models))
 monitor = collector.Monitor()
+# End: Instantiate models
 
 # Connect entities
 for model, agent in zip(models, agents):
     world.connect(model, agent, ("val", "val_in"))
     world.connect(agent, model, "delta", weak=True)
+# End: Connect entities
 
+# Connect to monitor
 mosaik.util.connect_many_to_one(world, models, monitor, "val", "delta")
 mosaik.util.connect_many_to_one(world, agents, monitor, "delta")
 
